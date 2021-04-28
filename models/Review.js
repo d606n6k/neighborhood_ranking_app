7cdf87b5aeef601ac7ceae3013257718035575dc
Arvin John Bautista <arvinyoung01@gmail.com>
2021-04-28 13:09:14 -0700

    Aside from models/Review.js added, seed/seed.js created as well, requiring (User,Neighborhood,Review) models, added reviewData.json

---
+++ models/Review.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Review extends Model {}
+
+Review.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    neighborhood_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "city",
+        key: "id",
+      },
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "review",
+  }
+);
+
+module.exports = Review;
