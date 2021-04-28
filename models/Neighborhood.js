b1ab45c7a80abab5a9f94d83c6bed76acc97b089
William Lucht <aaronlucht@gmail.com>
2021-04-28 13:31:22 -0700

    removed uneeded passwork encryption from Neighborhood.js model file

--- models/Neighborhood.js
+++ models/Neighborhood.js
@@ -3,12 +3,7 @@ const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
 // create our User model
-class Neighborhood extends Model {
-  // set up method to run on instance data (per user) to check password
-  checkPassword(loginPw) {
-    return bcrypt.compare(loginPw, this.password);
-  }
-}
+class Neighborhood extends Model {}
 
 Neighborhood.init(
   {
@@ -24,20 +19,6 @@ Neighborhood.init(
     },
   },
   {
-    hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(
-          updatedUserData.password,
-          10
-        );
-        return updatedUserData;
-      },
-    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
