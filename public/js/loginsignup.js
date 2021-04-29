677e7ea500efe6752d86f437c5f204ecdc436332
Arvin John Bautista <arvinyoung01@gmail.com>
2021-04-29 15:17:50 -0700

    Double checked, post working at Insomnia Core

--- public/js/loginsignup.js
+++ public/js/loginsignup.js
@@ -12,7 +12,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace("/profile"); // to be updated
+      document.location.replace("/profile");
     } else {
       alert(response.statusText);
     }
@@ -27,14 +27,14 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (username && email && password) {
-    const response = await fetch("/api/users", {
+    const response = await fetch("/api/users/", {
       method: "POST",
       body: JSON.stringify({ username, email, password }),
       headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace("/profile"); // to be updated
+      document.location.replace("/profile");
     } else {
       alert(response.statusText);
     }
