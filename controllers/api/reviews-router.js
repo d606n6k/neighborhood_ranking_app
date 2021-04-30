a38f91dd59b16c35adb1cc1fd9537e6d6a160734
Arvin John Bautista <arvinyoung01@gmail.com>
2021-04-30 09:32:45 -0700

    Added reviews-router backend, create and delete review routes

---
+++ controllers/api/reviews-router.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Review } = require('../../models');
+const withAuth = require('../../util/withAuth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newReview = await Review.create({
+      ...req.body,
+      user_id: req.session.userId,
+    });
+
+    res.status(200).json(newReview);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const reviewData = await Review.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.userId,
+      },
+    });
+
+    if (!reviewData) {
+      res.status(404).json({ message: 'No review found with this id!' });
+      return;
+    }
+
+    res.status(200).json(reviewData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
